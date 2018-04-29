1ea847db5610a0053a0e6a57df2b87e5fd332ba7
aGIToz <38216671+aGIToz@users.noreply.github.com>
2018-04-30 01:37:49 +0200

    Update myConfig.py

--- conf/myConfig.py
+++ conf/myConfig.py
@@ -1,14 +1,14 @@
 #import packages
 import os
 
+#default number of epochs
 epochs=50
+
+#default batch size for training
 batch_size=128
 
 #noise-level for training
-sigma=25.0
-
-#path to training data
-data='./trainingPatch/img_clean_pats.npy'
+sigma=25.0  #change it according to noise level in your dataset
 
 #path to generate the data
 genDataPath='./genData/'
@@ -16,9 +16,11 @@ genDataPath='./genData/'
 #path to save the genPatches
 save_dir='./trainingPatch/'
 
+#path to training data
+data='./trainingPatch/img_clean_pats.npy'
+
+#variables required to generate patch
 pat_size=40
-
 stride=10
-
 step=0
 
